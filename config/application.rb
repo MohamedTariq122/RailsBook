f49a58b20cf7adf2389521bc713c53610fb4fd07
MohamedTariq <mohamed.tairq19219@gmail.com>
2025-01-18 16:28:37 +0200

    creating posts

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,12 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# Load .env file in development and test environments
+if Rails.env.development? || Rails.env.test?
+  Dotenv::Rails.load
+end
+
+
 module RailsBook
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
